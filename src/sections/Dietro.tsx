85a0fcdd469fb75b1b09ef37563e5a98cfd449cc
Marco Sosio <marcososio007@gmail.com>
2025-09-04 20:21:20 +0200

    sistemato

--- src/sections/Dietro.tsx
+++ src/sections/Dietro.tsx
@@ -11,7 +11,7 @@ export default function Dietro() {
     return (
         <div className="self-center">
             <button
-                className={`text-sm text-neutral-400
+                className={`text-sm text-neutral-400 py-2 px-3 border-1 border-neutral-400 rounded-2xl
                     ${videoVisible && "hidden"}`}
                     onClick={()=>{showHideVideo()}}
             >
@@ -20,7 +20,7 @@ export default function Dietro() {
             <div className={`${!videoVisible && "hidden"} flex flex-col items-center`}>
                 <video src={video} className="w-60" controls autoPlay muted></video>
                 <button
-                    className="text-red-400 mt-2"
+                    className="text-red-400 mt-2 py-2 px-3 border-1 border-red-400 rounded-2xl"
                     onClick={() => {
                         showHideVideo();
                     }}
--- src/sections/Pensiero.tsx
+++ src/sections/Pensiero.tsx
@@ -4,10 +4,11 @@ export default function Pensiero() {
             Anche se sembrava un miraggio futuro, ci sei arrivata, ora sei
             maggiorenne. Legalmente hai raggiunto la maturità, dunque ti sono
             finalmente concessi diritti e libertà nuove, e per questo
-            festeggiamo. Ti auguro che tu possa approfittare di queste nuovo
-            possibilità sempre però guidati dalla responsabilità, più si è
-            liberi più bisogna tenersela stretta per trarre sempre i massimi
-            vantaggi he ti auguro sempre di poter ottenere.
+            festeggiamo. Ti auguro che tu possa approfittare di queste nuove
+            possibilità sempre però guidata dalla responsabilità che quanto più
+            si è liberi quanto più è necessario tenersela stretta per trarre da
+            ogni cosa sempre i massimi vantaggi che ti auguro sempre di poter
+            ottenere.
         </p>
     );
 }
